6d515f76a43e7e03644cd52585077ccbecd252da
СОМ <117897751+ILYA222213@users.noreply.github.com>
2024-06-28 17:41:40 +0300

    Update Logger.cs

--- Redactor/Redactor/Logger.cs
+++ Redactor/Redactor/Logger.cs
@@ -11,12 +11,16 @@ namespace Redactor
     {
         public static void Log(string message)
         {
+            // Получение пути к рабочему столу пользователя.
             string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+           // Создание пути к файлу журнала.
             string logFilePath = Path.Combine(desktopPath, "Message.log");
-
+// Открытие файла журнала для добавления в него сообщения.
             using (StreamWriter writer = File.AppendText(logFilePath))
             {
+                // Формирование записи в журнале с меткой времени.
                 string logEntry = $"[{DateTime.Now}] {message}";
+                // Запись сообщения в файл журнала.
                 writer.WriteLine(logEntry);
             }
         }
